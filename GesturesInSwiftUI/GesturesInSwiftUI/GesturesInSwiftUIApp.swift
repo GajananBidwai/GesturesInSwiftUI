94f59497a70ecc27ee1a1a8bae5266c97de7f249
Gajanan Bidwai <gajananbidwai525@gmail.com>
2025-12-12 12:10:47 +0530

    Initial Commit

---
+++ GesturesInSwiftUI/GesturesInSwiftUI/GesturesInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturesInSwiftUIApp.swift
+//  GesturesInSwiftUI
+//
+//  Created by Neosoft on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GesturesInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
