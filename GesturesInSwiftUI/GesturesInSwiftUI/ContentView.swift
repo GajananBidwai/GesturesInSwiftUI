a4ade576e025fba43307ad37d1d8d1bf6bc859c6
Gajanan Bidwai <gajananbidwai525@gmail.com>
2025-12-12 13:00:09 +0530

    On long gesture

--- GesturesInSwiftUI/GesturesInSwiftUI/ContentView.swift
+++ GesturesInSwiftUI/GesturesInSwiftUI/ContentView.swift
@@ -9,22 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     @State var isClick: Bool = false
+    @State var isPressing: Bool = false
     
     var body: some View {
         VStack {
             Image(.sample)
-            .onTapGesture {
-                location in
+                .opacity(isPressing ? 0 : 1)
+//            .onTapGesture {
+//                location in
+//                isClick = true
+//                print("Location \(location)")
+//            }
+            .onLongPressGesture(minimumDuration: 1, maximumDistance: 10 ,perform: {
                 isClick = true
-                print("Location \(location)")
-            }
+            }, onPressingChanged: { value in
+                withAnimation(.easeInOut(duration: 1.5)) {
+                    isPressing = value
+                }
+            })
+            
             .sheet(isPresented: $isClick) {
                 ShowImage()
+                
             }
-
         }
         .padding()
     }
+    
+    
 }
 
 #Preview {
